a8b023607495584e30e23b6539f732b7533d287b
sylvia326 <hxiaowei@uwo.ca>
2019-12-10 11:27:59 -0500

    Updates

---
+++ precache-manifest.dc0f787b70ef01127e8bb739d73cd823.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b22d9941936ea069824d1ed344b2e9a",
+    "url": "/react-searching-page/index.html"
+  },
+  {
+    "revision": "3d055dd82fc1e99d71c9",
+    "url": "/react-searching-page/static/css/main.eeea42d8.chunk.css"
+  },
+  {
+    "revision": "e612cb3dbedb23f6e8d9",
+    "url": "/react-searching-page/static/js/2.47434cba.chunk.js"
+  },
+  {
+    "revision": "3d055dd82fc1e99d71c9",
+    "url": "/react-searching-page/static/js/main.a9bda781.chunk.js"
+  },
+  {
+    "revision": "f8fdecaab4bebc59490c",
+    "url": "/react-searching-page/static/js/runtime-main.7c530388.js"
+  }
+]);
